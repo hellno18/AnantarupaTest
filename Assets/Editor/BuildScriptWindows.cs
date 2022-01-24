60ed53094972895fbac296d93bdbb2451cdc8b2d
Michael Jack <40555371+hellno18@users.noreply.github.com>
2022-01-24 11:55:36 +0700

    Update BuildScriptWindows.cs

--- Assets/Editor/BuildScriptWindows.cs
+++ Assets/Editor/BuildScriptWindows.cs
@@ -4,13 +4,13 @@ using UnityEditor.Build.Reporting;
 
 public class BuildScriptWindows : MonoBehaviour
 {
-    [MenuItem("Build/Build iOS")]
-    public static void MyBuild()
+    [MenuItem("Build/Build Android")]
+    public static void MyBuildAndroid()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/AnantarupaTest.unity" };
-        buildPlayerOptions.locationPathName = "WindowsBuild";
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.locationPathName = "build/android/AnantarupaTest.apk";
+        buildPlayerOptions.target = BuildTarget.Android;
         buildPlayerOptions.options = BuildOptions.None;
 
         BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
