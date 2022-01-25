13feec6d634b638aeb0e89a3348d84232ca46c64
Michael Jack <40555371+hellno18@users.noreply.github.com>
2022-01-25 17:24:06 +0700

    Update PerformBuild.cs

--- Assets/Editor/PerformBuild.cs
+++ Assets/Editor/PerformBuild.cs
@@ -9,21 +9,44 @@ namespace teamcity
     public class PerformBuild : MonoBehaviour
     {
         private const string PROJECT_NAME = "AnantarupaTest";
-
-        public static void ToAndroid()
+        
+        [MenuItem("Build/ Build To Android")]
+        public static void BuildToAndroid(bool increaseVersion)
         {
-            Build(".apk", BuildTarget.Android);
+            if (increaseVersion)
+            {
+                var bundleVersionSplit = UnityEditor.PlayerSettings.bundleVersion.Split('.');
+                int major = 0;
+                int minor = 0;
+                int subVersion = 0;
+                if (bundleVersionSplit.Length >= 1)
+                    int.TryParse(bundleVersionSplit[0], out major);
+                if (bundleVersionSplit.Length >= 2)
+                    int.TryParse(bundleVersionSplit[1], out minor);
+                if (bundleVersionSplit.Length >= 3)
+                    int.TryParse(bundleVersionSplit[2], out subVersion);
+                ++subVersion;
+                string version = string.Format("{0}.{1}.{2}", major, minor, subVersion);
+                var bundleVersionCode = (major * 100000) + (minor * 1000) + subVersion;
+
+                UnityEditor.PlayerSettings.bundleVersion = version;
+                UnityEditor.PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
+                UnityEditor.PlayerSettings.macOS.buildNumber = bundleVersionCode.ToString();
+            }
+                Build(".apk", BuildTarget.Android);
         }
 
-        public static void ToiOS()
+        public static void BuildToiOS()
         {
             Build(".app", BuildTarget.iOS);
         }
 
-        public static void ToWindows64()
+        public static void BuildToWindows64()
         {
             Build(".exe", BuildTarget.StandaloneWindows64);
         }
+
+        
 
         private static void Build(string nameDotExtentions, BuildTarget buildTarget)
         {
@@ -45,28 +68,7 @@ namespace teamcity
                 "Assets/Scenes/AnantarupaTest.unity",
             };
         }
-        [MenuItem("Build/icrease ver")]
-        public static string IncVersion()
-        {
-            var bundleVersionSplit = UnityEditor.PlayerSettings.bundleVersion.Split('.');
-            int major = 0;
-            int minor = 0;
-            int subVersion = 0;
-            if (bundleVersionSplit.Length >= 1)
-                int.TryParse(bundleVersionSplit[0], out major);
-            if (bundleVersionSplit.Length >= 2)
-                int.TryParse(bundleVersionSplit[1], out minor);
-            if (bundleVersionSplit.Length >= 3)
-                int.TryParse(bundleVersionSplit[2], out subVersion);
-            ++subVersion;
-            string version = string.Format("{0}.{1}.{2}", major, minor, subVersion);
-            var bundleVersionCode = (major * 100000) + (minor * 1000) + subVersion;
-
-            UnityEditor.PlayerSettings.bundleVersion = version;
-            UnityEditor.PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
-            UnityEditor.PlayerSettings.macOS.buildNumber = bundleVersionCode.ToString();
-            return version;
-        }
+        
 
         private static void DebugResult(BuildPlayerOptions buildPlayerOptions)
         {
